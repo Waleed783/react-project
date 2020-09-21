d43c3b90912cd2160be0134e52d5d27a645ab84c
Waleed783 <70505100+Waleed783@users.noreply.github.com>
2020-09-21 16:04:09 +0500

    code updated

---
+++ build/precache-manifest.f2ad06d2d566659e3b529d9d37d1170f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf79b74d3ed304b67751ee5bbada0d72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55e46a4860236eabb132",
+    "url": "/static/css/main.bc1a1419.chunk.css"
+  },
+  {
+    "revision": "04eb5684b65fd311e331",
+    "url": "/static/js/2.3590ac44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3590ac44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55e46a4860236eabb132",
+    "url": "/static/js/main.0e57e0d3.chunk.js"
+  },
+  {
+    "revision": "4fb080de24627b03831e",
+    "url": "/static/js/runtime-main.f16ea130.js"
+  }
+]);
